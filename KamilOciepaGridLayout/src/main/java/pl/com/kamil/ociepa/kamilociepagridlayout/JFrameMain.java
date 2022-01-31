bd116a00dc87e8674a18b6a2a55c2cb1dede0a32
KamilOciepos <kapeciu45@gmail.com>
2022-01-31 09:46:49 +0100

    es

--- KamilOciepaGridLayout/src/main/java/pl/com/kamil/ociepa/kamilociepagridlayout/JFrameMain.java
+++ KamilOciepaGridLayout/src/main/java/pl/com/kamil/ociepa/kamilociepagridlayout/JFrameMain.java
@@ -26,22 +26,263 @@ public class JFrameMain extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        ko_jTextFieldDzialania = new javax.swing.JTextField();
+        jPanel2 = new javax.swing.JPanel();
+        ko_jButton1 = new javax.swing.JButton();
+        ko_jButton2 = new javax.swing.JButton();
+        ko_jButton3 = new javax.swing.JButton();
+        ko_jButtonMinus = new javax.swing.JButton();
+        ko_jButton4 = new javax.swing.JButton();
+        ko_jButton5 = new javax.swing.JButton();
+        ko_jButton6 = new javax.swing.JButton();
+        ko_jButtonPlus = new javax.swing.JButton();
+        ko_jButton7 = new javax.swing.JButton();
+        ko_jButton8 = new javax.swing.JButton();
+        ko_jButton9 = new javax.swing.JButton();
+        ko_jButtonDzielenie = new javax.swing.JButton();
+        ko_jButtonUsun = new javax.swing.JButton();
+        ko_jButton0 = new javax.swing.JButton();
+        ko_jButtonWynik = new javax.swing.JButton();
+        ko_jButtonMnozenie = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        ko_jTextFieldDzialania.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        ko_jTextFieldDzialania.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addComponent(ko_jTextFieldDzialania, javax.swing.GroupLayout.PREFERRED_SIZE, 383, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(40, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(ko_jTextFieldDzialania, javax.swing.GroupLayout.DEFAULT_SIZE, 74, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        jPanel2.setLayout(new java.awt.GridLayout(4, 4));
+
+        ko_jButton1.setText("1");
+        ko_jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ko_jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ko_jButton1);
+
+        ko_jButton2.setText("2");
+        ko_jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ko_jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ko_jButton2);
+
+        ko_jButton3.setText("3");
+        ko_jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ko_jButton3ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ko_jButton3);
+
+        ko_jButtonMinus.setText("-");
+        ko_jButtonMinus.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ko_jButtonMinusActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ko_jButtonMinus);
+
+        ko_jButton4.setText("4");
+        ko_jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ko_jButton4ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ko_jButton4);
+
+        ko_jButton5.setText("5");
+        ko_jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ko_jButton5ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ko_jButton5);
+
+        ko_jButton6.setText("6");
+        ko_jButton6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ko_jButton6ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ko_jButton6);
+
+        ko_jButtonPlus.setText("+");
+        ko_jButtonPlus.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ko_jButtonPlusActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ko_jButtonPlus);
+
+        ko_jButton7.setText("7");
+        ko_jButton7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ko_jButton7ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ko_jButton7);
+
+        ko_jButton8.setText("8");
+        ko_jButton8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ko_jButton8ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ko_jButton8);
+
+        ko_jButton9.setText("9");
+        ko_jButton9.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ko_jButton9ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ko_jButton9);
+
+        ko_jButtonDzielenie.setText("/");
+        ko_jButtonDzielenie.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ko_jButtonDzielenieActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ko_jButtonDzielenie);
+
+        ko_jButtonUsun.setText("C");
+        ko_jButtonUsun.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ko_jButtonUsunActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ko_jButtonUsun);
+
+        ko_jButton0.setText("0");
+        ko_jButton0.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ko_jButton0ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ko_jButton0);
+
+        ko_jButtonWynik.setText("=");
+        ko_jButtonWynik.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ko_jButtonWynikActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ko_jButtonWynik);
+
+        ko_jButtonMnozenie.setText("*");
+        ko_jButtonMnozenie.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ko_jButtonMnozenieActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ko_jButtonMnozenie);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+    
+    String temp = "";
+    private void ko_jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ko_jButton2ActionPerformed
+        
+    }//GEN-LAST:event_ko_jButton2ActionPerformed
 
+    private void ko_jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ko_jButton1ActionPerformed
+        
+    }//GEN-LAST:event_ko_jButton1ActionPerformed
+
+    private void ko_jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ko_jButton3ActionPerformed
+        
+    }//GEN-LAST:event_ko_jButton3ActionPerformed
+
+    private void ko_jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ko_jButton4ActionPerformed
+        
+    }//GEN-LAST:event_ko_jButton4ActionPerformed
+
+    private void ko_jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ko_jButton5ActionPerformed
+        
+    }//GEN-LAST:event_ko_jButton5ActionPerformed
+
+    private void ko_jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ko_jButton6ActionPerformed
+        
+    }//GEN-LAST:event_ko_jButton6ActionPerformed
+
+    private void ko_jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ko_jButton7ActionPerformed
+        
+    }//GEN-LAST:event_ko_jButton7ActionPerformed
+
+    private void ko_jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ko_jButton8ActionPerformed
+        
+    }//GEN-LAST:event_ko_jButton8ActionPerformed
+
+    private void ko_jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ko_jButton9ActionPerformed
+        
+    }//GEN-LAST:event_ko_jButton9ActionPerformed
+
+    private void ko_jButton0ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ko_jButton0ActionPerformed
+        
+    }//GEN-LAST:event_ko_jButton0ActionPerformed
+
+    private void ko_jButtonPlusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ko_jButtonPlusActionPerformed
+        
+    }//GEN-LAST:event_ko_jButtonPlusActionPerformed
+
+    private void ko_jButtonMinusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ko_jButtonMinusActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ko_jButtonMinusActionPerformed
+
+    private void ko_jButtonDzielenieActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ko_jButtonDzielenieActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ko_jButtonDzielenieActionPerformed
+
+    private void ko_jButtonMnozenieActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ko_jButtonMnozenieActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ko_jButtonMnozenieActionPerformed
+
+    private void ko_jButtonWynikActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ko_jButtonWynikActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ko_jButtonWynikActionPerformed
+
+    private void ko_jButtonUsunActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ko_jButtonUsunActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ko_jButtonUsunActionPerformed
+    int liczba1;
+    int liczba2;
+    String znak = "";
     /**
      * @param args the command line arguments
      */
@@ -78,5 +319,24 @@ public class JFrameMain extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JButton ko_jButton0;
+    private javax.swing.JButton ko_jButton1;
+    private javax.swing.JButton ko_jButton2;
+    private javax.swing.JButton ko_jButton3;
+    private javax.swing.JButton ko_jButton4;
+    private javax.swing.JButton ko_jButton5;
+    private javax.swing.JButton ko_jButton6;
+    private javax.swing.JButton ko_jButton7;
+    private javax.swing.JButton ko_jButton8;
+    private javax.swing.JButton ko_jButton9;
+    private javax.swing.JButton ko_jButtonDzielenie;
+    private javax.swing.JButton ko_jButtonMinus;
+    private javax.swing.JButton ko_jButtonMnozenie;
+    private javax.swing.JButton ko_jButtonPlus;
+    private javax.swing.JButton ko_jButtonUsun;
+    private javax.swing.JButton ko_jButtonWynik;
+    private javax.swing.JTextField ko_jTextFieldDzialania;
     // End of variables declaration//GEN-END:variables
 }
